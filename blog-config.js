d2d5b52836d2a693a40d77e982e971b2df79017e
hh.jang <fallear1@gmail.com>
2022-10-29 01:14:55 +0900

    feat: 3차 배포

--- blog-config.js
+++ blog-config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  title: "현호의 기술 블로그",
+  title: "현호의 기술블로그",
   description: "Hello :)",
   author: "Hyunho Jang",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://hyunolike.github.io/",
   links: {
     github: "https://github.com/hyunolike",
-    email: "fallear1@gmail.com",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
